ba2eb21f8b91b207a467fdd527ddb4335f918eef
Bernard De Freitas <bernard30101994@gmail.com>
2025-05-19 12:19:17 -0300

    Agregar implementación de clases y archivos de configuración para el desafío de control de flujo

--- desafios/controle-fluxo/src/ParametrosInvalidosException.java
+++ desafios/controle-fluxo/src/ParametrosInvalidosException.java
@@ -1,4 +1,7 @@
 package src;
 
-public class ParametrosInvalidosException {
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
 }
